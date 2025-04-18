17d62da5c259f304aadbb80e4ae90b173259ea2a
Lee Gyeongju <rudwn969@naver.com>
2025-04-18 20:31:32 +0900

    modify attr and result data transplant

--- app/image_detector.py
+++ app/image_detector.py
@@ -1,24 +1,22 @@
 from ultralytics import YOLO
-from app.detect_result import DetectResult
+from numpy import ndarray
+from app.detect_result import DetectResultBox
 
 #이미지를 감지할 YOLO 모델이 정의된 클래스스
 class ImageDetector:
     def __init__(self):
         self.__model = YOLO("yolov8n.pt")
-       #self.__confidence: float = 0.5
-       # self.__idDict <- id를 label string으로 바꿀 dict가 들어감
+        self.__idDict = {"hello" : 3}
 
     #이미지 url을 받아 감지후 결과를 DetectResult 리스트로 반환환
-    def DetectImages(self, image: str) -> list[DetectResult]:
+    def DetectingImage(self, img: ndarray, confidence: float) -> list[DetectResultBox]:
         detectresults = []
-        results = self.__model(image)
-
-        print(results[0].boxes.cls)
-        print(results[0].boxes.xyxy)
+        results = self.__model(img)
 
         for box in results[0].boxes:
-            detectresult = DetectResult(image,int(box.cls), box.xyxy.tolist() , 0.5)
-
+            xyxy = box.xyxy.tolist()
+            int_xyxy = [int(num) for num in xyxy[0]]
+            detectresult = DetectResultBox(int(box.cls), tuple(int_xyxy))
             detectresults.append(detectresult)
 
         return detectresults
