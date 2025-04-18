48b4343827c17c203b5630f78b50a85208541c5f
Lee Gyeongju <rudwn969@naver.com>
2025-04-18 20:29:32 +0900

    restructure

--- app/detect_result.py
+++ app/detect_result.py
@@ -1,8 +1,6 @@
 from dataclasses import dataclass
 # 이미지 감지 결과를 저장
 @dataclass
-class DetectResult:
-    url: str
+class DetectResultBox:
     label: int
-    box: list[float, float, float, float]
-    confidence: float
+    box: tuple[int, int, int, int]
