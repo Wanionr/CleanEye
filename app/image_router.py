a3cb617796d03ca314ffbc50f45fcbbf4eb55aba
Lee Gyeongju <rudwn969@naver.com>
2025-04-18 20:32:29 +0900

    add image router(get and return JSON)

---
+++ app/image_router.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter
+from pydantic import BaseModel
+from app.image_processor import ImageProcessor
+
+router = APIRouter()
+processor = ImageProcessor()
+
+class ImageRequest(BaseModel):
+    image_urls: list[str]
+    confidence: float
+
+class ImageResponse(BaseModel):
+    results: list[str]
+
+@router.post("/detect", response_model=ImageResponse)
+async def process_images(request: ImageRequest):
+    results: list[str] = []
+    
+    for url in request.image_urls:
+        result_image = processor.ImageProcess(url, request.confidence)
+        results.append(result_image)
+        
+    return {"results": results}
