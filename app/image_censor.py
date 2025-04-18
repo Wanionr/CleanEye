3b727445cd04825dec40ce57f66037e660501b5c
Lee Gyeongju <rudwn969@naver.com>
2025-04-18 20:29:57 +0900

    add blur

--- app/image_censor.py
+++ app/image_censor.py
@@ -1,8 +1,30 @@
-#from app.detect_result import DetectResult
+import numpy as np
+import cv2 as cv
+from app.detect_result import DetectResultBox
 
 class ImageCensor:
-    def apply(self, image):
-        for l in image:
-            print(l.url)
-            print(l.label)
-            print(l.box)
+    def CensoringImage(self, img: np.ndarray, boxes: list[DetectResultBox]):
+        """주어진 이미지와 좌표 박스에 검열 처리를 합니다.
+
+        Args:
+            img(np.ndarray): 검열 작업이 필요한 이미지
+            boxes(list[DetectResultBox]): 이미지에서 감지된 부적절한 부분의 좌표 박스 리스트
+
+        Returns:
+            np.ndarray: 
+        
+        """
+        result_img = img.copy()
+        strength = (5, 5)
+        for box in boxes:
+            x_min, y_min, x_max, y_max = map(int, box.box)
+            roi = img[y_min:y_max, x_min:x_max]
+            censored_roi = self.__ImageBlur(roi, strength)
+            result_img[y_min:y_max, x_min:x_max] = censored_roi
+
+        return result_img
+    
+    def __ImageBlur(self, roi: np.ndarray, blur_strength: tuple[int, int]):
+        censored_roi = cv.GaussianBlur(roi, blur_strength, 0)
+
+        return censored_roi
